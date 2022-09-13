280a93c8265c4c84a882da318149dd73584a28c9
Cristobal Sanchez Serrano <cristobal.sanchez.serrano@emeal.nttdata.com>
2022-09-13 13:52:08 +0200

    feat()_ Añaida la pagina de lista de producto.

--- product-app/src/views/productList/ProductList.jsx
+++ product-app/src/views/productList/ProductList.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import ProductCard from './productCard/ProductCard';
+import './productList.css';
 
 const ProductList = () => {
-  const { products } = useSelector((state) => state);
-  console.log('products: ', products);
+  const { products } = useSelector((state) => state.products);
+  const navigate = useNavigate();
 
-  return <div>ProductList</div>;
+  const navigateToProductDetail = (id) => {
+    navigate(`/products/${id}`, { replace: true });
+  };
+
+  return (
+    <div className="product-list-container">
+      {products.map((product) => (
+        <ProductCard
+          key={product.id}
+          id={product.id}
+          title={product.brand}
+          subTitle={product.model}
+          price={product.price}
+          image={product.imgUrl}
+          onClick={navigateToProductDetail}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default ProductList;
